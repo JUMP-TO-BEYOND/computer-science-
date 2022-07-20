5d3cda6225567578775a0262683aba9b3c127618
khj <kim37171@cuk.edu>
2022-07-20 18:38:15 +0900

    [HJ] Doubly Linked List 구현

--- src/hj/datastructure/list/DoublyLinkedList.java
+++ src/hj/datastructure/list/DoublyLinkedList.java
@@ -1,7 +1,5 @@
 package hj.datastructure.list;
 
-import ys.datastructure.list.indexException;
-
 import java.util.*;
 import java.util.List;
 
@@ -45,6 +43,11 @@ public class DoublyLinkedList<E> implements List<E> {
         public void setValue(E value) {
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
     }
 
     @Override
@@ -62,6 +65,20 @@ public class DoublyLinkedList<E> implements List<E> {
 
     @Override
     public boolean contains(Object o) {
+        //list의 크기가 0일 때
+        if(head == null){
+            throw new EmptyStackException();
+        }
+
+        int tmpIndex = 0;
+        Node<E> searchNode = head;
+        while(tmpIndex <= size-1) {
+            if (searchNode.getValue().equals(o)) {
+                return true;
+            }
+            searchNode = searchNode.next;
+            tmpIndex++;
+        }
         return false;
     }
 
@@ -93,25 +110,93 @@ public class DoublyLinkedList<E> implements List<E> {
         }
 
         //size가 0이 아니면 마지막 노드에 추가
-        addLast(e);
+        Node<E> newNode = new Node<>(tail, null, e);
+        tail.next = newNode;
+        tail = newNode;
         size++;
         return true;
     }
 
+    /*
+    add로 처리 가능할 듯?
     private void addLast(E e) {
         Node<E> newNode = new Node<>(tail, null, e);
         tail.next = newNode;
         tail = newNode;
     }
+    */
 
     private void addFirst(E e) {
         Node<E> newNode = new Node<>(null,head,e);
         head = newNode;
     }
 
+    // TODO: 2022-07-20 중복 데이터의 경우, 첫 번째 일치하는 data를 제거한다. 
     @Override
-    public boolean remove(Object o) {
+    public boolean remove(Object o) throws RuntimeException {
+
+        //list의 크기가 0일 때
+        if(head == null){
+            throw new EmptyStackException();
+        }
+
+        int tmpIndex = 0;
+        Node<E> searchNode = head;
+        while(tmpIndex <= size-1){
+            if(searchNode.getValue().equals(o)){
+                //첫 노드 삭제인 경우
+                if(tmpIndex == 0){
+                    removeFirst();
+                    return true;
+                }
+
+                //마지막 노드 삭제인 경우
+                if(tmpIndex == size-1){
+                    removeLast();
+                    return true;
+                }
+                searchNode.previous.setNext(searchNode.next);
+                searchNode.next.setPrevious(searchNode.previous);
+                size--;
+                return true;
+            }
+            searchNode = searchNode.next;
+            tmpIndex++;
+        }
+
         return false;
+    }
+
+    private void removeLast() {
+        //크기가 1인 경우
+        if(head.next == null){
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+
+        //크기가 1이상인 경우
+        Node<E> previousNode = tail.previous;
+        previousNode.next = null;
+        tail = previousNode;
+        size--;
+    }
+
+    private void removeFirst() {
+        //크기가 1인 경우
+        if(head.next == null){
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+
+        //크기가 1이상인 경우
+        Node<E> nextNode = head.next;
+        nextNode.previous = null;
+        head = nextNode;
+        size--;
     }
 
     @Override
@@ -141,12 +226,17 @@ public class DoublyLinkedList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        if(head == null){
+            throw new EmptyStackException();
+        }
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
     public E get(int index) {
-        return getNode(index).value;
+        return getNode(index).getValue();
     }
 
     @Override
@@ -165,14 +255,6 @@ public class DoublyLinkedList<E> implements List<E> {
             return;
         }
 
-        //index == size-1 (마지막 노드 삽입)
-        if(index == size-1){
-            addLast(element);
-            size++;
-            return;
-        }
-
-        // (중간 삽입)
         Node<E> tmpNode = getNode(index);
         Node<E> previousNode = tmpNode.previous;
         Node<E> newNode = new Node<>(previousNode,tmpNode,element);
@@ -208,21 +290,38 @@ public class DoublyLinkedList<E> implements List<E> {
 
     }
 
-    private void validateIndex(int index) {
+    private void validateIndex(int index) throws RuntimeException{
         //index validation
         //0 <= index < size
-        try {
-            if(!(0 <= index && index < size)) {
-                throw new indexException();
-            }
-        } catch (indexException e) {
-            throw new RuntimeException(e);
+        if(!(0 <= index && index < size)) {
+            throw new IndexOutOfBoundsException();
         }
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        validateIndex(index);
+        E removeData = null;
+        //첫 노드 삭제인 경우
+        if(index == 0){
+            removeData = head.getValue();
+            removeFirst();
+            return removeData;
+        }
+
+        //마지막 노드 삭제인 경우
+        if(index == size-1){
+            removeData = tail.getValue();
+            removeLast();
+            return removeData;
+        }
+
+        Node<E> searchNode = getNode(index);
+        removeData = searchNode.getValue();
+        searchNode.previous.setNext(searchNode.next);
+        searchNode.next.setPrevious(searchNode.previous);
+        size--;
+        return removeData;
     }
 
     @Override
@@ -250,5 +349,20 @@ public class DoublyLinkedList<E> implements List<E> {
         return null;
     }
 
-
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<E> currentNode = head;
+        for(int i = 0; i < size; i++){
+            if(i == size - 1){
+                str += currentNode.getValue();
+                str += "]";
+                return str;
+            }
+            str += currentNode.getValue() + " , ";
+            currentNode = currentNode.getNext();
+        }
+        str += "]";
+        return str;
+    }
 }
